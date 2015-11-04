6c913e6eb33f96c39000e68269c129f3cea32019
yuntao.qyt <yuntao.qyt@alibaba-inc.com>
2015-11-04 22:26:30 +0800

    Initial Commit

---
+++ my-weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  my-weibo
+//
+//  Created by qyt on 11/4/15.
+//  Copyright © 2015 qyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
